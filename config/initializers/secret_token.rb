50847b1d7c5c88493578b2dc1b991dbb0242528e
Josh Teng <joshteng@webermize.com>
2013-05-10 03:34:35 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LaunchPage::Application.config.secret_token = 'b4545184a477738d274b1510f789538758deb73d29e5cafcabb2c5c4a4980150cc587c48edcd391ff4abe6a5836d22c2274ae49f17642e0a14d1a006b732a8ff'
